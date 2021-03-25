a45030ec0c8018c823be452e8bb12accc6d6f451
tannerfronk <tannerjace07@hotmail.com>
2021-03-24 18:24:19 -0600

    Initial commit, created WorkoutList component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WorkoutList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WorkoutList from "./components/WorkoutList";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WorkoutList
   }
 }
 </script>
---
+++ src/components/WorkoutList.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Hello World real</h2>
+</template>
+
+<script>
+    export default {
+        name: "WorkoutList"
+    }
+</script>
+
+<style scoped>
+
+</style>
